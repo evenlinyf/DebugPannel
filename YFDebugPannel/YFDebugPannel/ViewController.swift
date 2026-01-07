193272f9b672b0cf82aaa4ab424e7dc6f75e5887
linyunfang <linyunfang@hikvision.com.cn>
2026-01-07 19:55:27 +0800

    init commit

---
+++ YFDebugPannel/YFDebugPannel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YFDebugPannel
+//
+//  Created by linyunfang on 2026/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
